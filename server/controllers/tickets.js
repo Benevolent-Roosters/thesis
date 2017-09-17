e8bc062b22d54be9b2099f49078758344492e564
Daniel Cervoni <dcervoni03@gmail.com>
2017-09-16 17:06:45 -0700

    implemented tests for new cli server routes

--- server/controllers/tickets.js
+++ server/controllers/tickets.js
@@ -9,12 +9,39 @@ module.exports.getPanelTickets = (req, res) => {
   }
   //req.query.panel_id used to be req.params.panel_id but axios GET can only put in req.query
   var panelId = req.query.panel_id;
-  dbhelper.getTicketsByPanel(parseInt(panelId))
+  dbhelper.getTicketsByPanel(panelId)
     .then(tickets => {
       if (!tickets) {
         throw 'cant get tickets by panel id';
       }
       res.status(200).send(tickets);
+    })
+    .catch(err => {
+      // This code indicates an outside service (the database) did not respond in time
+      res.status(500).send(JSON.stringify(err));
+    });
+};
+
+/** TODO: WRITE TEST FOR THIS FUNCTION **/
+module.exports.getPanelTicketsByUser = (req, res) => {
+  if (helper.checkUndefined(req.query.panel_id)) {
+    res.status(400).send('one of parameters from client is undefined');
+    return;
+  }
+  
+  var panelId = req.query.panel_id;
+  dbhelper.getTicketsByPanel(panelId)
+    .then(tickets => {
+      if (!tickets) {
+        throw 'cant get tickets by panel id';
+      }
+      
+      //filter for user tickets
+      let userTickets = tickets.filter(ticket => {
+        return ticket.assignee_handle === req.query.github_handle;
+      });
+
+      res.status(200).send(userTickets.reverse());
     })
     .catch(err => {
       // This code indicates an outside service (the database) did not respond in time
@@ -96,6 +123,28 @@ module.exports.getOneTicket = (req, res) => {
       res.status(200).send(ticket);
     })
     .catch((err) => {
+      res.status(500).send(JSON.stringify(err));
+    });
+};
+
+/** WRITE TEST FOR THIS ROUTE **/
+module.exports.getUserTicketsByBoard = (req, res) => {
+  if (helper.checkUndefined(req.query.user_id, req.query.github_handle, req.query.board_id, req.query.api_key)) {
+    res.status(400).send('one of parameters from client is undefined');
+    return;
+  }
+  
+  var userHandle = req.query.github_handle;
+  var boardId = req.query.board_id;
+  dbhelper.getTicketsByUserHandleAndBoard(userHandle, boardId)
+    .then(tickets => {
+      if (!tickets) {
+        throw 'cant get tickets by panel id';
+      }
+      res.status(200).send(tickets);
+    })
+    .catch(err => {
+      // This code indicates an outside service (the database) did not respond in time
       res.status(500).send(JSON.stringify(err));
     });
 };
